b465806a9a51deae559287c2ead210605c8f9423
John Brunton <john_brunton@hotmail.co.uk>
2014-01-11 17:00:15 +0000

    #11 scaffolding for blog posts

---
+++ spec/views/blog_posts/new.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "blog_posts/new" do
+  before(:each) do
+    assign(:blog_post, stub_model(BlogPost,
+      :title => "MyString",
+      :content => "MyText"
+    ).as_new_record)
+  end
+
+  it "renders new blog_post form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", blog_posts_path, "post" do
+      assert_select "input#blog_post_title[name=?]", "blog_post[title]"
+      assert_select "textarea#blog_post_content[name=?]", "blog_post[content]"
+    end
+  end
+end
