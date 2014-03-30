22ab7d3b52e9a6a6858da9f2fb29b03ea36ee371
John Brunton <john_brunton@hotmail.co.uk>
2014-03-30 11:55:22 +0100

    #108 update friendly_id on blog_posts after updating title

--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe BlogPost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#friendly_id" do
+    it "has a friendly_id" do
+      blog_post = create(:blog_post, title: 'Some Post')
+      expect(blog_post.friendly_id).to eq('1-some-post')
+    end
+    
+    it "updates the friendly_id when the title is changed" do
+      blog_post = create(:blog_post, title: 'Some Title')
+      expect(blog_post.friendly_id).to eq('1-some-title')
+
+      blog_post.title = 'Some Other Title'
+      blog_post.save
+      expect(blog_post.friendly_id).to eq('1-some-other-title')
+    end
+  end
 end
