3360e0b0170683c21cc0719594bb5c9c85b60ea4
John Brunton <john_brunton@hotmail.co.uk>
2014-04-21 13:10:25 +0100

    #125 refactored specs

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,18 +2,29 @@ require 'spec_helper'
 
 describe Ability do
   context "Users" do
-    it "grants read permissions for published posts to all users" do
-      user = create(:user)
-      ability = Ability.new(user)
-      blog_post = create(:blog_post, published: true)
-      assert ability.can?(:read, blog_post)
+    subject(:ability) { Ability.new(user) }
+    
+    context "for any user" do
+      let(:user) { create(:user) }
+      
+      it "grants read permissions for published posts" do
+        blog_post = create(:blog_post, published: true)
+        assert ability.can?(:read, blog_post)        
+      end
+      
+      it "restricts reading of draft posts" do
+        blog_post = create(:blog_post, published: false)
+        assert ability.cannot?(:read, blog_post)                
+      end
     end
     
-    it "restricts reading draft posts to admins" do
-      user = create(:user)
-      ability = Ability.new(user)
-      blog_post = create(:blog_post, published: false)
-      assert ability.cannot?(:read, blog_post)
+    context "for admins" do
+      let(:user) { create(:user, :admin) }
+      
+      it "grants read permissions for draft posts" do
+        blog_post = create(:blog_post, published: false)
+        assert ability.can?(:read, blog_post)                
+      end
     end
   end
 end
