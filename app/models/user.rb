946954fc6cec8d5442ea3328272218116b171363
John Brunton <john_brunton@hotmail.co.uk>
2014-11-27 22:24:03 +0000

    updated dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   
   def self.find_for_facebook_oauth(auth)
-    where(auth.slice(:uid)).first_or_create do |user|
+    where(uid: auth["uid"]).first_or_create do |user|
       user.uid = auth.uid
       user.name = auth.info.name
     end
